a13069a13cd9e040d7be5d1bd6173bedf94b97ee
blkgoose <alessio.biancone@gmail.com>
2023-05-12 14:33:34 +0200

    improvement: clean init

--- init.lua
+++ init.lua
@@ -12,35 +12,35 @@ opt.incsearch = true
 opt.completeopt = "menu"
 opt.autoindent = true
 opt.expandtab = true
-opt.shiftwidth = 4
-opt.smarttab = true
-opt.softtabstop = 4
 opt.foldmethod = "indent"
-opt.relativenumber = true
 
-opt.laststatus = 3
+opt.laststatus = 0
 opt.guicursor = "i:block"
 opt.termguicolors = true
 opt.pumheight = 12
 
 opt.mouse = ""
 
+cmd([[
+    let s:pair_mode = 0
+    function! PairMode()
+        if s:pair_mode  == 1
+            let s:pair_mode = 1
+            set norelativenumber
+        else
+            let s:pair_mode = 0
+            set relativenumber
+        endif
+    endfunction
+
+    nnoremap <leader>P :call PairMode()<CR>
+]])
+
 nmap("cf", ":!touch <cfile><cr>")
-nmap("Y", "yy")
 nmap("*", '"syiw<Esc>: let @/ = @s<CR>:set hlsearch<CR>')
 nmap("<F1>", ':let @/ = ""<CR>')
 
-cmd([[ inoremap <C-c> USA ESC... ]])
-
 opt.clipboard = "unnamed,unnamedplus"
-
--- reload conf automatically
--- cmd([[
--- augroup conf_update
---   autocmd!
---   autocmd BufWritePost $HOME/.config/nvim/* lua reload_conf()
--- augroup end
--- ]])
 
 -- highlight what has been copied
 cmd([[
@@ -48,7 +48,12 @@ autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', 
 ]])
 
 cmd([[
-autocmd BufRead * normal zR
+autocmd BufWinEnter * normal zR
+]])
+
+cmd([[
+nnoremap <C-c> [{V%
+xnoremap <C-c> "_y[{V%
 ]])
 
 require("plugins")
