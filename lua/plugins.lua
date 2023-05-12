91f43e02ba1da06ace231fae7eb93fadcb42a12a
blkgoose <alessio.biancone@gmail.com>
2023-05-12 14:34:18 +0200

    improvement: use rose_pine as colorscheme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,7 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 local palette = require("palette")
 local colors = palette.colors
-local syntax = palette.syntax
 
 require("lazy").setup({
   -- fuzzy search
@@ -31,84 +30,10 @@ require("lazy").setup({
 
   -- themes
   {
-    "themercorp/themer.lua",
-    -- event = "VeryLazy",
+    "ThemerCorp/themer.lua",
     opts = {
-      colorscheme = {
-        red = colors.dark_red,
-        yellow = colors.yellow,
-        orange = colors.dark_yellow,
-        magenta = colors.magenta,
-        blue = colors.blue,
-        green = colors.green,
-        cyan = colors.cyan,
-
-        directory = colors.blue,
-        fg = colors.white,
-        diff = {
-          add = colors.dark_green,
-          remove = colors.dark_red,
-          text = colors.dark_blue,
-          change = colors.dark_yellow,
-        },
-        accent = colors.cyan,
-        search_result = { fg = colors.dark_black, bg = colors.blue, telescope = colors.blue },
-        match = colors.grey9,
-        dimmed = { bg = colors.grey4, fg = colors.grey4 },
-        bg = {
-          base = colors.black,
-          alt = colors.light_black,
-          selected = colors.grey2,
-        },
-        border = colors.white,
-        syntax = syntax,
-        built_in = syntax,
-        diagnostic = {
-          error = colors.strong_red,
-          warn = colors.strong_yellow,
-          info = colors.strong_green,
-          hint = colors.magenta,
-        },
-        inc_search = { fg = colors.dark_black, bg = colors.blue },
-        uri = colors.dark_yellow,
-        pum = {
-          fg = colors.grey9,
-          bg = colors.dark_black,
-          sbar = colors.light_black,
-          thumb = colors.blue,
-          sel = { bg = colors.grey2, fg = colors.white },
-        },
-        heading = {
-          h1 = colors.blue,
-          h2 = colors.blue,
-        },
-        remaps = {
-          base = {
-            winseparator = { fg = colors.grey2, bg = colors.black },
-            diffadd = { fg = colors.dark_green, bg = colors.black },
-            diffchange = { fg = colors.dark_yellow, bg = colors.black },
-            diffdelete = { fg = colors.dark_red, bg = colors.black },
-          },
-          plugins = {
-            nvim_tree = {
-              nvimtreenormal = { fg = colors.white, bg = colors.dark_black },
-              nvimtreegitdeleted = { fg = colors.red },
-              nvimtreegitdirty = { fg = colors.yellow },
-              nvimtreegitstaged = { fg = colors.green },
-              nvimtreegitmerge = { fg = colors.yellow },
-              nvimtreegitnew = { fg = colors.red },
-              nvimtreegitrenamed = { fg = colors.white },
-              nvimtreespecialfile = { fg = colors.white },
-              nvimtreeimagefile = { fg = colors.white },
-              nvimtreeopenedfile = { fg = colors.white },
-              nvimtreemodifiedfile = { fg = colors.white },
-              nvimtreeindentmarker = { fg = colors.white },
-              nvimtreesymlink = { fg = colors.white },
-              nvimtreeexecfile = { fg = colors.white },
-            },
-          },
-        },
-      },
+      colorscheme = "rose_pine",
+      diagnostic_underline = "underline",
     },
   },
 
