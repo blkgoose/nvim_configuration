19a3c182d4dcd75705669207f983952953e8310f
blkgoose <alessio.biancone@gmail.com>
2023-05-12 14:36:23 +0200

    improvement: remove useless gitsigns confs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -136,54 +136,18 @@ require("lazy").setup({
   -- git commands
   {
     "tpope/vim-fugitive",
-    config = function()
-      nmap("<leader>b", ":Git blame<CR>")
-      nmap("<leader>gm", "/<<<<<<<\\|=======\\|>>>>>>><CR>")
-    end,
+    keys = {
+      { "<leader>b", ":Git blame<CR>" },
+      { "<leader>gm", "/<<<<<<<\\|=======\\|>>>>>>><CR>" },
+      { "<leader>gs", ":Gvdiffsplit<CR>" },
+    },
   },
 
   {
     "lewis6991/gitsigns.nvim",
     event = "VeryLazy",
     opts = {
-      signs = {
-        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      },
-      signcolumn = true,
       numhl = true,
-      linehl = false,
-      word_diff = false,
-      watch_gitdir = {
-        interval = 1000,
-        follow_files = true,
-      },
-      attach_to_untracked = true,
-      current_line_blame = false,
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "right_align",
-        delay = 1000,
-        ignore_whitespace = false,
-      },
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-      sign_priority = 6,
-      update_debounce = 100,
-      status_formatter = nil,
-      max_file_length = 10000,
-      preview_config = {
-        border = "single",
-        style = "minimal",
-        relative = "cursor",
-        row = 0,
-        col = 1,
-      },
-      yadm = {
-        enable = false,
-      },
     },
   },
 
